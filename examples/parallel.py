d61e93b229a75266a9c7e087851d011f4d96fd20
Max Kruse <maximilian.kruse@kit.edu>
2025-02-12 11:26:28 +0100

    docs: comment examples

--- examples/parallel.py
+++ examples/parallel.py
@@ -12,6 +12,7 @@ from pysde import increments, runner, schemes, storages
 
 
 # ==================================================================================================
+# Define drift and diffusion functions i.t.o space and time
 def drift(x, t):
     return -2 * np.power(x, 3) + 3 * x
 
@@ -20,6 +21,7 @@ def diffusion(x, t):
     return np.sqrt(np.power(x, 2) + 2)
 
 
+# Define initial condition and solver parameters
 rng = np.random.default_rng(seed=0)
 x0 = rng.normal(loc=0, scale=0.5, size=(1, 100000))
 t0 = 0.0
@@ -27,6 +29,7 @@ dt = 0.01
 num_steps = 101
 
 
+# Set up parallel runner with component types and respective constructor arguments
 sde_runner = runner.ParallelRunner(
     drift_function=drift,
     diffusion_function=diffusion,
@@ -39,5 +42,7 @@ sde_runner = runner.ParallelRunner(
     save_directory=Path("../results_example_parallel/result"),
     avoid_race_condition=True,
 )
+
+# Solve SDE and save results
 storage = sde_runner.run(x0, t0, dt, num_steps, progress_bar=True)
 storage.save()
